a3b7354aa473262f5f6556b2b2951a41d607663b
Junsu <ljs980630123@gmail.com>
2021-09-01 17:46:55 +0900

    차박지 리스트 테스트 코드 작성

--- src/Screens/ChabakjiList.js
+++ src/Screens/ChabakjiList.js
@@ -10,8 +10,29 @@ import {
   FlatList,
   Image,
 } from 'react-native';
+import SelectDropdown from 'react-native-select-dropdown';
 
+const sort = ['최신순', '오래된순'];
 const screenWidth = Dimensions.get('window').width;
+
+const DATA = [
+  {
+    title: '차박지1',
+    recommendcount: '5',
+    starcount: '★★★★★',
+  },
+  {
+    title: '차박지2',
+    recommendcount: '1',
+    starcount: '★★★★☆',
+  },
+  {
+    title: '차박지3',
+    recommendcount: '1',
+    starcount: '★★★☆☆',
+  },
+];
+
 const ChabakjiList = ({navigation}) => {
   const [searchText, setSearchText] = useState('');
 
@@ -48,14 +69,72 @@ const ChabakjiList = ({navigation}) => {
           <Text style={{color: 'white', fontSize: 18}}>검색</Text>
         </TouchableOpacity>
       </View>
+      <View
+        style={{justifyContent: 'center', alignItems: 'center', marginTop: 20}}>
+        <SelectDropdown
+          buttonStyle={{
+            width: 130,
+            height: 50,
+            borderColor: '#295eba',
+            borderWidth: 2,
+            borderRadius: 8,
+            backgroundColor: 'white',
+          }}
+          buttonTextStyle={{fontSize: 17}}
+          data={sort}
+          defaultValue={'최신순'}
+          onSelect={(selectedItem, index) => {}}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem;
+          }}
+          rowTextForSelection={(item, index) => {
+            return item;
+          }}
+        />
+      </View>
       <View style={styles.List}>
-        <TouchableOpacity
-          style={{backgroundColor: 'black'}}
-          onPress={() => {
-            navigation.navigate('ChabakjiInfo');
-          }}>
-          <Text>ddd</Text>
-        </TouchableOpacity>
+        <FlatList
+          data={DATA}
+          keyExtractor={(item, index) => {
+            return `ChabakjiList-${index}`;
+          }}
+          renderItem={({item, index}) => (
+            <TouchableOpacity
+              style={{
+                width: screenWidth / 1.1,
+                height: 70,
+                backgroundColor: '#295eba',
+                marginBottom: 10,
+                borderRadius: 12,
+                justifyContent: 'center',
+              }}
+              onPress={() => {
+                navigation.navigate('ChabakjiInfo');
+              }}>
+              <Text
+                style={{
+                  marginLeft: 10,
+                  fontSize: 18,
+                  fontWeight: 'bold',
+                  color: 'white',
+                }}>
+                {item.title}
+              </Text>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginTop: 7,
+                }}>
+                <Text style={{marginLeft: 10, color: 'white'}}>
+                  추천 수 : {item.recommendcount}
+                </Text>
+                <Text style={{position: 'absolute', right: 10, color: 'white'}}>
+                  별점 : {item.starcount}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          )}
+        />
       </View>
     </SafeAreaView>
   );
@@ -63,8 +142,8 @@ const ChabakjiList = ({navigation}) => {
 
 const styles = StyleSheet.create({
   Search: {
+    marginTop: 20,
     flexDirection: 'row',
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -89,9 +168,10 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 8,
   },
   List: {
-    flex: 6,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: 10,
   },
 });
 export default ChabakjiList;
