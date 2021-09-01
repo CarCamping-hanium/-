7810695fc685bdfa54256ba1827d81e4fc15d55c
Junsu <ljs980630123@gmail.com>
2021-09-01 17:49:29 +0900

    리뷰리스트 디자인 변경

--- src/Screens/ReviewBoard.js
+++ src/Screens/ReviewBoard.js
@@ -8,61 +8,30 @@ import {
   TouchableOpacity,
   Image,
   View,
+  Dimensions,
 } from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
+
+const screenWidth = Dimensions.get('window').width;
 const sort = ['추천순', '최신순', '오래된순'];
 const DATA = [
   {
     title: '리뷰제목1',
-    recommandcount: '5',
+    recommendcount: '5',
     starcount: '★★★★★',
   },
   {
     title: '리뷰제목2',
-    recommandcount: '1',
+    recommendcount: '1',
     starcount: '★★★★☆',
   },
   {
     title: '리뷰제목3',
-    recommandcount: '1',
+    recommendcount: '1',
     starcount: '★★★☆☆',
   },
 ];
 const ReviewBoard = ({navigation}) => {
-  const Item = ({item, onPress, backgroundColor, textColor}) => (
-    <TouchableOpacity
-      style={[styles.Item, backgroundColor]}
-      onPress={() => {
-        navigation.navigate('ReviewInfo');
-      }}>
-      <Text style={[styles.Title, textColor]}>{item.title}</Text>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-        }}>
-        <Text style={[styles.RecommendCount, textColor]}>
-          추천수 : {item.recommandcount}
-        </Text>
-        <Text style={[styles.Starcount, textColor]}>
-          별점 : {item.starcount}
-        </Text>
-      </View>
-    </TouchableOpacity>
-  );
-  const [selectedId, setSelectedId] = useState(null);
-  const renderItem = ({item}) => {
-    const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#295eba';
-    const color = item.id === selectedId ? 'white' : 'white';
-    return (
-      <Item
-        item={item}
-        onPress={() => setSelectedId(item.id)}
-        backgroundColor={{backgroundColor}}
-        textColor={{color}}
-      />
-    );
-  };
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => (
@@ -116,11 +85,42 @@ const ReviewBoard = ({navigation}) => {
       <FlatList
         style={{marginTop: 20}}
         data={DATA}
-        renderItem={renderItem}
+        renderItem={({item, index}) => (
+          <TouchableOpacity
+            style={{
+              width: screenWidth / 1.1,
+              height: 70,
+              backgroundColor: '#295eba',
+              marginBottom: 10,
+              borderRadius: 12,
+              justifyContent: 'center',
+            }}>
+            <Text
+              style={{
+                marginLeft: 10,
+                fontSize: 18,
+                fontWeight: 'bold',
+                color: 'white',
+              }}>
+              {item.title}
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 7,
+              }}>
+              <Text style={{marginLeft: 10, color: 'white'}}>
+                추천 수 : {item.recommendcount}
+              </Text>
+              <Text style={{position: 'absolute', right: 10, color: 'white'}}>
+                별점 : {item.starcount}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
         keyExtractor={(item, index) => {
           return `review-${index}`;
         }}
-        extraData={selectedId}
       />
 
       <View
@@ -142,23 +142,8 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
     backgroundColor: 'white',
-  },
-  Item: {
-    padding: 15,
-    marginVertical: 5,
-    marginHorizontal: 16,
-    borderRadius: 8,
-  },
-  Title: {
-    fontSize: 15,
-  },
-  RecommendCount: {
-    fontSize: 12,
-    textAlign: 'left',
-  },
-  Starcount: {
-    fontSize: 12,
-    textAlign: 'right',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   MainTitle: {
     fontSize: 25,
