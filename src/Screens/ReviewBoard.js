a3b91aa358c4b3adb2a609a5a4ff34e40005e19f
2Junsu <ijh1205@naver.com>
2021-09-23 11:44:11 +0900

    리뷰리스트랑 등록버튼 겹쳐보이는 현상 수정

--- src/Screens/ReviewBoard.js
+++ src/Screens/ReviewBoard.js
@@ -13,7 +13,6 @@ import {
 import SelectDropdown from 'react-native-select-dropdown';
 import {UserContext} from '../Context/Context';
 import {useFocusEffect} from '@react-navigation/native';
-import {interpolateNode} from 'react-native-reanimated';
 const screenWidth = Dimensions.get('window').width;
 const sort = ['별점 높은순', '별점 낮은순', '최신순', '오래된순'];
 const ReviewBoard = ({navigation}) => {
@@ -74,6 +73,7 @@ const ReviewBoard = ({navigation}) => {
           marginTop: 20,
           flexDirection: 'row',
           justifyContent: 'center',
+          flex: 1,
         }}>
         <Text style={styles.Sort}>정렬 : </Text>
         <SelectDropdown
@@ -104,67 +104,70 @@ const ReviewBoard = ({navigation}) => {
           }}
         />
       </View>
-
-      <FlatList
-        style={{marginTop: 20}}
-        data={ReviewList}
-        renderItem={({item, index}) => (
-          <TouchableOpacity
-            style={{
-              width: screenWidth / 1.1,
-              height: 70,
-              backgroundColor: '#295eba',
-              marginBottom: 10,
-              borderRadius: 12,
-              justifyContent: 'center',
-            }}
-            onPress={() => {
-              selectedReview_ID(item.review_id);
-              selectedReview_name(item.title);
-              navigation.navigate('ReviewInfo');
-            }}>
-            <View style={{flexDirection: 'row'}}>
-              <Text
-                style={{
-                  marginLeft: 10,
-                  fontSize: 18,
-                  fontWeight: 'bold',
-                  color: 'white',
-                }}>
-                {item.title}
-              </Text>
-              <Text
-                style={{
-                  position: 'absolute',
-                  right: 10,
-                  marginLeft: 10,
-                  fontSize: 15,
-                  color: 'white',
-                }}>
-                작성자 : {item.writer}
-              </Text>
-            </View>
-            <View
+      <View style={{flex: 8}}>
+        <FlatList
+          data={ReviewList}
+          renderItem={({item, index}) => (
+            <TouchableOpacity
               style={{
-                flexDirection: 'row',
-                marginTop: 7,
+                width: screenWidth / 1.1,
+                height: 70,
+                backgroundColor: '#295eba',
+                marginBottom: 10,
+                borderRadius: 12,
+                justifyContent: 'center',
+              }}
+              onPress={() => {
+                selectedReview_ID(item.review_id);
+                selectedReview_name(item.title);
+                navigation.navigate('ReviewInfo');
               }}>
-              <Text style={{marginLeft: 10, color: 'white'}}>
-                추천 수 : {item.recommend}
-              </Text>
-              <Text style={{position: 'absolute', right: 10, color: 'white'}}>
-                별점 : {item.score}
-              </Text>
-            </View>
-          </TouchableOpacity>
-        )}
-        keyExtractor={(item, index) => {
-          return `review-${index}`;
-        }}
-      />
-
+              <View style={{flexDirection: 'row'}}>
+                <Text
+                  style={{
+                    marginLeft: 10,
+                    fontSize: 18,
+                    fontWeight: 'bold',
+                    color: 'white',
+                  }}>
+                  {item.title}
+                </Text>
+                <Text
+                  style={{
+                    position: 'absolute',
+                    right: 10,
+                    marginLeft: 10,
+                    fontSize: 15,
+                    color: 'white',
+                  }}>
+                  작성자 : {item.writer}
+                </Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  marginTop: 7,
+                }}>
+                <Text style={{marginLeft: 10, color: 'white'}}>
+                  추천 수 : {item.recommend}
+                </Text>
+                <Text style={{position: 'absolute', right: 10, color: 'white'}}>
+                  별점 : {item.score}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(item, index) => {
+            return `review-${index}`;
+          }}
+        />
+      </View>
       <View
-        style={{justifyContent: 'center', alignItems: 'center', bottom: 40}}>
+        style={{
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+          flex: 1,
+        }}>
         <TouchableOpacity
           style={styles.ReviewUpload}
           onPress={() => {
