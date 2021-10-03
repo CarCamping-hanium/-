b93a76e67fe32aee18c2d0beb8fd5535266b173f
2Junsu <ijh1205@naver.com>
2021-10-04 03:08:18 +0900

    고객센터 페이지 추가

---
+++ src/Screens/CustomerService.js
@@ -0,0 +1,25 @@
+import React, {
+  useState,
+  useContext,
+  useLayoutEffect,
+  useEffect,
+  useCallback,
+} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  TextInput,
+  Alert,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+
+const CustomerService = () => {
+  return <Text>개발자에게 문의하기</Text>;
+};
+
+export default CustomerService;
